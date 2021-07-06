64418d8361a6ceb0096ef3dcfc12de9bc5e22edd
nhanphan1159 <nhanphan1159@gmail.com>
2021-07-06 15:25:06 +0700

    Add project files.

---
+++ tuan4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tuan4.Startup))]
+namespace tuan4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
